5ac0cb341c02c6ff9943296946da3ff5fc3ea078
Ilya Kirsanov <kirsanovia@yandex.ru>
2022-05-09 00:37:19 +0300

    fix: correct webpack config

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -3,19 +3,23 @@ const path = require('path');
 const dotenv = require('dotenv');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+function getEnvValues() {
+  const fileEnv = dotenv.config().parsed;
+  return Object
+    .keys(fileEnv)
+    .reduce((envs, key) => {
+      envs[`process.env.${key}`] = JSON.stringify(fileEnv[key]);
+      return envs;
+    }, {});
+}
+
 module.exports = () => {
   const mode = process.env.NODE_ENV || 'development';
   const fileEnv = dotenv.config().parsed;
 
   const target = mode === 'development' ? 'web' : 'browserslist';
   const devtool = mode === 'development' ? 'inline-source-map' : 'source-map';
-
-  const envKeys = Object
-    .keys(fileEnv)
-    .reduce((envs, key) => {
-      envs[`process.env.${key}`] = JSON.stringify(fileEnv[key]);
-      return envs;
-    }, {});
+  const envKeys = mode === 'development' ? getEnvValues() : {};
 
   return {
     mode,
