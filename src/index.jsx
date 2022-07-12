b837727354c20b26d90d321d548493ee6c5e035c
Кирсанов Илья Александрович (vtb4047222) <iakirsanov@vtb.ru>
2022-07-12 20:55:40 +0300

    fix: comment mocking

--- src/index.jsx
+++ src/index.jsx
@@ -14,11 +14,11 @@ import '../assets/application.scss';
 import '../assets/index.scss';
 
 // Start the mocking and debugging conditionally
-if (process.env.NODE_ENV === 'development') {
-  localStorage.debug = 'chat:*';
-  const { worker } = require('./mocks/browser.js');
-  worker.start();
-}
+// if (process.env.NODE_ENV === 'development') {
+//   localStorage.debug = 'chat:*';
+//   const { worker } = require('./mocks/browser.js');
+//   worker.start();
+// }
 
 ReactDOM.render(
   <React.StrictMode>
